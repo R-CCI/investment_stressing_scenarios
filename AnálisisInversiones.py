ebd3e9585211bb7b332ed867eee76f7135699c40
R-CCI <ymejia@cci.com.do>
2025-11-21 11:20:36 -0400

    Update AnálisisInversiones.py

--- AnálisisInversiones.py
+++ AnálisisInversiones.py
@@ -273,9 +273,12 @@ if uploaded_file is not None:
     cashflow_fideico = (new_cf*fideico)
     aporte_inicial_fideico = st.number_input("Aporte Inicial Fideicomitente", value=-2163.3, step=0.1)
     cashflow_fideico.iloc[0] = aporte_inicial_fideico
+
+    st.write("Flujo Fideicomitente")
+    st.dataframe(pd.DataFrame((cashflow_fideico).T, hide_index=True)
     
     cashflow_opv = (new_cf*ofp)
-    aporte_inicial_opv = st.number_input("Aporte Inicial Fideicomitente", value=-441.00, step=0.1)
+    aporte_inicial_opv = st.number_input("Aporte Inicial Publico General", value=-441.00, step=0.1)
     cashflow_opv.iloc[1] = aporte_inicial_opv
 
     fideico_irr = irr(cashflow_fideico)
@@ -324,6 +327,7 @@ if uploaded_file is not None:
 
 else:
     st.info("Suba un Excel")
+
 
 
 
